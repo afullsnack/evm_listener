bef6a288c840bd78f8055d2c7a99a85306f5ed04
Miracle Friday <miraclef60@gmail.com>
2024-09-14 12:52:26 +0100

    refactor: Refactor webhook handler
    - Install @effect/platform
    - Refactor webhook handler
    - Refactor entry point

--- index.ts
+++ index.ts
@@ -3,22 +3,24 @@ import { publicListenerClient } from "./src/client"
 import tokenAbi from "./abi/ERC20Token.json"
 import type {Abi} from "viem"
 import {Effect} from "effect"
-import { handleLogs, policy } from "./src/handler"
+import { handleLogs } from "./src/handler"
 
 // TODO: create and manage listener for contract
 // > call webhook when event emitted
 
-console.log(Bun.env.WEBHOOK_PROXY_PAYLOAD, ':::webhook proxy')
+// console.log(Bun.env.WEBHOOK_PROXY_PAYLOAD, ':::webhook proxy')
 
 const watchInstance = publicListenerClient.watchContractEvent({
   address: '0x2Dda0158EFe7006B8aFf9e5FEe6E9112F16841D8',
   eventName: 'Transfer',
   abi: tokenAbi?.abi as Abi,
   args: { to: '0x0000000000000000000000000000000000000000' },
-  onLogs: logs => handleLogs(logs).pipe(Effect.retry({ times: 3}), Effect.runPromise),
+  onLogs: logs => handleLogs(logs)
+    .pipe(
+      Effect.retry({ times: 3}),
+      Effect.runPromise
+    ),
   onError: error => console.log(error, "::: error on listening") // setup a graceful shutdown and restart on error, based on the type of error
 })
 
-
-console.log(watchInstance, ":::watcher started")
-console.log(Bun.env.NODE_ENV, ":::node env")
+console.log("Watcher started.....");
--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 import { formatUnits, type Log } from "viem"
 import { Effect, Schedule } from "effect"
+import { HttpClient, HttpClientRequest, HttpClientResponse } from "@effect/platform";
 
 
 // TODO: create retry policy
@@ -12,25 +13,44 @@ export const policy = Schedule.fixed("100 millis")
 export const handleLogs = (logs: Log[]) => Effect.gen(function*() {
   yield* Effect.log('Event received....', logs[0])
 
-  const callWebhook = yield* Effect.tryPromise({
-    try: () => fetch(
-      Bun.env.NODE_ENV === "development"
-        ? Bun.env.WEBHOOK_PROXY_PAYLOAD ?? "https://smee.io/f29WKHg05Pg0u4dT"
-          : Bun.env.MAIN_API_WEBHOOK ?? "https://staging.api.wrapcbdc.com/api/services/blockchain/events/evm/burn",
-      { method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(
-          logs[0],
-          // TODO: normalize bigint bofore calling webhook
-          //> instead of converting to string.
-          (_, v) => typeof v === 'bigint' ? formatUnits(v, 6) : v
-        )
-      }).then((res) => res.json()),
-    catch: () => Effect.fail(new Error("Error calling webhook forwarder"))
-  })
 
-  yield* Effect.log(callWebhook, ":::webhook called")
+  yield* HttpClientRequest.post(
+    Bun.env.NODE_ENV === "development"
+      ? Bun.env.WEBHOOK_PROXY_PAYLOAD ?? "https://smee.io/f29WKHg05Pg0u4dT"
+      : Bun.env.MAIN_API_WEBHOOK ?? "https://staging.api.wrapcbdc.com/api/services/blockchain/events/evm/burn"
+  ).pipe(
+    HttpClientRequest.setHeader("Content-type", "application/json charset=UTF-8;"),
+    // HttpClientRequest.acceptJson,
+    HttpClientRequest.textBody(
+      JSON.stringify(
+        logs[0],
+        // TODO: normalize bigint bofore calling webhook
+        //> instead of converting to string.
+        (_, v) => typeof v === 'bigint' ? formatUnits(v, 6) : v
+      )
+    ),
+    HttpClient.fetch,
+    HttpClientResponse.json,
+    Effect.tap((response) => console.log(response, ":::webhoook call")),
+  )
 
-  yield* Effect.log("Webhook call finished...")
+  // const callWebhook = yield* Effect.tryPromise({
+  //   try: () => fetch(
+  //     Bun.env.NODE_ENV === "development"
+  //       ? Bun.env.WEBHOOK_PROXY_PAYLOAD ?? "https://smee.io/f29WKHg05Pg0u4dT"
+  //       : Bun.env.MAIN_API_WEBHOOK ?? "https://staging.api.wrapcbdc.com/api/services/blockchain/events/evm/burn",
+  //     {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(
+  //         logs[0],
+  //         // TODO: normalize bigint bofore calling webhook
+  //         //> instead of converting to string.
+  //         (_, v) => typeof v === 'bigint' ? formatUnits(v, 6) : v
+  //       )
+  //     }).then((res) => res.json()),
+  //   catch: () => Effect.fail(new Error("Error calling webhook forwarder"))
+  // })
 
+  // yield* Effect.log(callWebhook, ":::webhook called")
 })
