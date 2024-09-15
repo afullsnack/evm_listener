1aab0a0576852b3c186fd3b2a5a921eac933f54a
Miracle Friday <miraclef60@gmail.com>
2024-09-15 03:07:13 +0100

    refactor: Revert changes back to normal fetch request

--- src/handler.ts
+++ src/handler.ts
@@ -14,43 +14,43 @@ export const handleLogs = (logs: Log[]) => Effect.gen(function*() {
   yield* Effect.log('Event received....', logs[0])
 
 
-  yield* HttpClientRequest.post(
-    Bun.env.NODE_ENV === "development"
-      ? Bun.env.WEBHOOK_PROXY_PAYLOAD ?? "https://smee.io/f29WKHg05Pg0u4dT"
-      : Bun.env.MAIN_API_WEBHOOK ?? "https://staging.api.wrapcbdc.com/api/services/blockchain/events/evm/burn"
-  ).pipe(
-    HttpClientRequest.setHeader("Content-type", "application/json charset=UTF-8;"),
-    // HttpClientRequest.acceptJson,
-    HttpClientRequest.textBody(
-      JSON.stringify(
-        logs[0],
-        // TODO: normalize bigint bofore calling webhook
-        //> instead of converting to string.
-        (_, v) => typeof v === 'bigint' ? formatUnits(v, 6) : v
-      )
-    ),
-    HttpClient.fetch,
-    HttpClientResponse.json,
-    Effect.tap((response) => console.log(response, ":::webhoook call")),
-  )
+  // yield* HttpClientRequest.post(
+  //   Bun.env.NODE_ENV === "development"
+  //     ? Bun.env.WEBHOOK_PROXY_PAYLOAD ?? "https://smee.io/f29WKHg05Pg0u4dT"
+  //     : Bun.env.MAIN_API_WEBHOOK ?? "https://staging.api.wrapcbdc.com/api/services/blockchain/events/evm/burn"
+  // ).pipe(
+  //   HttpClientRequest.setHeader("Content-type", "application/json charset=UTF-8;"),
+  //   // HttpClientRequest.acceptJson,
+  //   HttpClientRequest.textBody(
+  //     JSON.stringify(
+  //       logs[0],
+  //       // TODO: normalize bigint bofore calling webhook
+  //       //> instead of converting to string.
+  //       (_, v) => typeof v === 'bigint' ? formatUnits(v, 6) : v
+  //     )
+  //   ),
+  //   HttpClient.fetch,
+  //   HttpClientResponse.json,
+  //   Effect.tap((response) => console.log(response, ":::webhoook call")),
+  // )
 
-  // const callWebhook = yield* Effect.tryPromise({
-  //   try: () => fetch(
-  //     Bun.env.NODE_ENV === "development"
-  //       ? Bun.env.WEBHOOK_PROXY_PAYLOAD ?? "https://smee.io/f29WKHg05Pg0u4dT"
-  //       : Bun.env.MAIN_API_WEBHOOK ?? "https://staging.api.wrapcbdc.com/api/services/blockchain/events/evm/burn",
-  //     {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(
-  //         logs[0],
-  //         // TODO: normalize bigint bofore calling webhook
-  //         //> instead of converting to string.
-  //         (_, v) => typeof v === 'bigint' ? formatUnits(v, 6) : v
-  //       )
-  //     }).then((res) => res.json()),
-  //   catch: () => Effect.fail(new Error("Error calling webhook forwarder"))
-  // })
+  const callWebhook = yield* Effect.tryPromise({
+    try: () => fetch(
+      Bun.env.NODE_ENV === "development"
+        ? Bun.env.WEBHOOK_PROXY_PAYLOAD ?? "https://smee.io/f29WKHg05Pg0u4dT"
+        : Bun.env.MAIN_API_WEBHOOK ?? "https://staging.api.wrapcbdc.com/api/services/blockchain/events/evm/burn",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(
+          logs[0],
+          // TODO: normalize bigint bofore calling webhook
+          //> instead of converting to string.
+          (_, v) => typeof v === 'bigint' ? formatUnits(v, 6) : v
+        )
+      }).then((res) => res.json()),
+    catch: () => Effect.fail(new Error("Error calling webhook forwarder"))
+  })
 
-  // yield* Effect.log(callWebhook, ":::webhook called")
+  yield* Effect.log(callWebhook, ":::webhook called")
 })
